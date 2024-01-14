b64d2265aae61510eba476ef8ea7fc171ef9f540
Yosip Mike Colin <frontend@agrak.com>
2024-01-13 19:40:37 -0500

    Diseño de landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,131 @@
-import Image from 'next/image'
+import Image from "next/image";
+import styles from "../css/page.module.css";
+import Logo from "../../public/logo.png";
+import Imagen1 from "../../public/image-1.jpg";
+import { Poppins } from "next/font/google";
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css';
+const poppins = Poppins({ weight: "600", subsets: ["latin"] });
+const poppins2 = Poppins({ weight: "500", subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <section className={styles.container}>
+      <header>
+        <nav className={styles.menu}>
+          <ul>
+            <li>
+              <Image src={Logo} alt="logo" width={40} />
+            </li>
+            <li>(+57) 3103435659</li>
+          </ul>
+        </nav>
+      </header>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <main>
+        <section className={styles["front-page"]}>
+          <div className={styles.title}>
+            <h1>VENTA DE LOTES - CAMPO JOSHUA ESTEBAN</h1>
+            <h2>VILLANUEVA CASANARE</h2>
+            <p>El mejor lugar para inventir en tu futuro.</p>
+            <div className={styles["container-buttons"]}>
+              <button>Contacto</button>
+            </div>
+          </div>
+        </section>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <section className={styles["container-benefits"]}>
+          <h1 className={poppins2.className}>
+            BENEFICIOS DE COMPRAR EN CAMPOS JOSHUA ESTEBAN
+          </h1>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <section className={styles["container-box"]}>
+            <div className={styles.box}>
+              <Image
+                src={Imagen1}
+                alt="Imagen 1"
+                style={{ borderRadius: 10, height: 500, objectFit: "cover" }}
+              />
+              <h3 className={poppins.className}>Carretera pavimentada</h3>
+              <p>
+                Disfrute de un acceso sin complicaciones con nuestras
+                ubicaciones estratégicas que cuentan con carreteras pavimentadas
+                de alta calidad. Conectarse con la ciudad y otros destinos será
+                rápido y cómodo, facilitando su día a día y ofreciendo un
+                transporte eficiente para usted y su familia.
+              </p>
+            </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+            <div className={styles.box}>
+              <img
+                src={Imagen1.src}
+                alt="Imagen 1"
+                style={{ borderRadius: 10, height: 500, objectFit: "cover" }}
+              />
+              <h3 className={poppins.className}>Negocios de comida</h3>
+              <p>
+                Sumérjase en una experiencia culinaria diversa sin tener que
+                viajar lejos. Nuestros lotes están estratégicamente ubicados
+                cerca de una variedad de negocios de comida, desde restaurantes
+                locales hasta vibrantes mercados. Deléitese con la conveniencia
+                de tener opciones gastronómicas a su alcance.
+              </p>
+            </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+            <div className={styles.box}>
+              <Image
+                src={Imagen1}
+                alt="Imagen 1"
+                style={{ borderRadius: 10, height: 500, objectFit: "cover" }}
+              />
+              <h3 className={poppins.className}>Rio</h3>
+              <p>
+                Descubra la serenidad y la belleza natural de tener un río cerca
+                de su hogar. Desde vistas panorámicas hasta actividades
+                acuáticas, nuestro entorno fluvial ofrece una conexión única con
+                la naturaleza. Disfrute de paseos relajantes, pesca o
+                simplemente sumérjase en la tranquilidad que proporciona la
+                proximidad a un río.
+              </p>
+            </div>
+
+            <div className={styles.box}>
+              <Image
+                src={Imagen1}
+                alt="Imagen 1"
+                style={{ borderRadius: 10, height: 500, objectFit: "cover" }}
+              />
+              <h3 className={poppins.className}>Cerca de la ciudad</h3>
+              <p>
+                Experimente lo mejor de ambos mundos: la tranquilidad de la vida
+                suburbana y la accesibilidad de la vida en la ciudad. Nuestros
+                lotes están estratégicamente ubicados cerca de la ciudad,
+                brindándole la comodidad de acceder a servicios, entretenimiento
+                y oportunidades culturales sin sacrificar la paz y la privacidad
+                que ofrece un entorno más rural.
+              </p>
+            </div>
+
+            <div className={styles.box}>
+              <Image
+                src={Imagen1}
+                alt="Imagen 1"
+                style={{ borderRadius: 10, height: 500, objectFit: "cover" }}
+              />
+              <h3 className={poppins.className}>Piscilagos</h3>
+              <p>
+                Refresque su vida cotidiana con la proximidad a un parque de
+                agua. Perfecto para el entretenimiento familiar, estos espacios
+                ofrecen diversión acuática, áreas de recreación y un ambiente
+                relajado. Con un parque de agua cerca, siempre tendrá la opción
+                de disfrutar de momentos emocionantes y refrescantes sin tener
+                que alejarse demasiado de casa.
+              </p>
+            </div>
+          </section>
+        </section>
+
+      </main>
+    </section>
+  );
 }
