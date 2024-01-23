94face9e26b001039c7766c6a65df5bddb650ac2
Yosip Mike Colin <frontend@agrak.com>
2024-01-23 14:47:12 -0500

    Ajustes de icono y nombre

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,12 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
-import Head from "next/head";
 
 const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Lotes en villanueva casanare",
-  description: "Lotes en villanueva casanare",
+  title: "Lotes en Villanueva Casanare",
+  description: "lotes en villanueva casanare",
 };
 
 export default function RootLayout({
